84c46213b45de87d2583302c1cdff31fe94986cc
Jake St. Germain <jakesaint27@gmail.com>
2025-05-20 23:05:08 -0400

    minor config tweaks

--- .ScratchPad/PersistDataTest/src/env.ts
+++ .ScratchPad/PersistDataTest/src/env.ts
@@ -6,7 +6,10 @@ const DATA_SOURCE: string = (process.env.PORT) || 'sqlite';
 
 const FORCE_UPDATE: boolean = ((process.env.FORCE_UPDATE) || 'false').toLowerCase() == 'true';
 
+const BATCH_SIZE: number = parseInt(process.env.FORCE_UPDATE);
+
 export const Env = {
     DATA_SOURCE,
-    FORCE_UPDATE
+    FORCE_UPDATE,
+    BATCH_SIZE
 } as const;
