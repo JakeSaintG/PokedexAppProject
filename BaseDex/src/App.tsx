c880fd44786be9cdcd79618d91aa93652240af39
Jake St. Germain <jakesaint27@gmail.com>
2025-07-05 22:57:00 -0400

    try to set up linux shortcuts

--- BaseDex/src/App.tsx
+++ BaseDex/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { HomePage } from './HomePage';
 import { StartPage } from './StartPage';
 
 export default function App() {
     return <div className="App">
         Vite APP
         <StartPage value='test'></StartPage>
+        <HomePage value='also test'></HomePage>
     </div>;
 }
