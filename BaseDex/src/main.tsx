4f2b2ebcc65e959ab16d09b7b61e9f507c68c6f2
JakeSaintG <jakesaint27@gmail.com>
2025-07-09 22:03:53 -0400

    continue working on base dex routing

--- BaseDex/src/main.tsx
+++ BaseDex/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Welcome } from './components/Welcome';
+import { HomePage } from './components/HomePage';
+import { LoadingContentPage } from './components/LoadingContentPage';
+import App from './App';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path="/" element={<App />}>
+          <Route index element={<Welcome />} />
+          <Route path='/home' element={<HomePage value="routed to home page" />} />
+          <Route path="loading" element={<LoadingContentPage />} />
+      </Route>
+  )
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
