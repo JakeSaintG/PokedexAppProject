b3aecd8231dd04089e4ff7ea62f0d5ef0e17da7e
JakeSaintG <jakesaint27@gmail.com>
2025-07-10 23:59:47 -0400

    work on nav bar

--- BaseDex/src/components/NavigationMenu/NavigationMenu.tsx
+++ BaseDex/src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,9 +1,32 @@
 import styles from './NavigationMenu.module.css';
 
+// svgs below are courtesy of font awesome (free)
+// downloading the svgs directly is likely safer and decreases the number of packages needed
+import listUlSolid from "../../assets/list-ul-solid.svg";
+import gearSolid from "../../assets/gear-solid.svg";
+import userSolid from "../../assets/user-solid.svg";
+import houseSolid from "../../assets/house-solid.svg";
+
 export function NavigationMenu() {
     return (
-        <div className={styles.nav_menu}>
-            
-        </div>
+        <nav className={styles.nav_menu}>
+            <ul role="menubar">
+                <li role="menuitem">
+                    <img src={userSolid} alt="user icon for profile" className={styles.nav_img} height='40'/>
+                </li>
+                <li role="menuitem">
+                    <img src={gearSolid} alt="gear icon for settings" className={styles.nav_img} height='40'/>
+                </li>
+                <li className={styles.home_button} role="menuitem">
+                    <img src={houseSolid} alt="house icon for returning to home page" className={styles.nav_img} height='40'/>
+                </li>
+                <li role="menuitem">
+                    <img src={listUlSolid} alt="list icon for pokedex list view" className={styles.nav_img} height='40'/>
+                </li>
+                <li role="menuitem">
+                    <img src={gearSolid} alt="gear icon for settings" className={styles.nav_img} height='40'/>
+                </li>
+            </ul>
+        </nav>
     );
 }
