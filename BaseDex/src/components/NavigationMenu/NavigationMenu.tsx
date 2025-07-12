364dda085c59f2635f30f0cc4b7253aae040eb8c
JakeSaintG <jakesaint27@gmail.com>
2025-07-11 23:24:52 -0400

    handmake a grass icon that I can be proud of! :)

--- BaseDex/src/components/NavigationMenu/NavigationMenu.tsx
+++ BaseDex/src/components/NavigationMenu/NavigationMenu.tsx
@@ -4,6 +4,7 @@ import styles from './NavigationMenu.module.css';
 // downloading the svgs directly is likely safer and decreases the number of packages needed
 import listUlSolid from "../../assets/list-ul-solid.svg";
 import gearSolid from "../../assets/gear-solid.svg";
+import grassSolid from "../../assets/grass.svg";
 import userSolid from "../../assets/user-solid.svg";
 import houseSolid from "../../assets/house-solid.svg";
 
@@ -12,19 +13,19 @@ export function NavigationMenu() {
         <nav className={styles.nav_menu}>
             <ul role="menubar">
                 <li role="menuitem">
-                    <img src={userSolid} alt="user icon for profile" className={styles.nav_img} height='40'/>
+                    <img src={userSolid} alt="user icon for profile" className={styles.nav_img} height='35'/>
                 </li>
                 <li role="menuitem">
-                    <img src={gearSolid} alt="gear icon for settings" className={styles.nav_img} height='40'/>
+                    <img src={grassSolid} alt="grass icon for collecting pokemon" className={styles.nav_img} height='35'/>
                 </li>
                 <li className={styles.home_button} role="menuitem">
-                    <img src={houseSolid} alt="house icon for returning to home page" className={styles.nav_img} height='40'/>
+                    <img src={houseSolid} alt="house icon for returning to home page" className={styles.nav_img} height='35'/>
                 </li>
                 <li role="menuitem">
-                    <img src={listUlSolid} alt="list icon for pokedex list view" className={styles.nav_img} height='40'/>
+                    <img src={listUlSolid} alt="list icon for pokedex list view" className={styles.nav_img} height='35'/>
                 </li>
                 <li role="menuitem">
-                    <img src={gearSolid} alt="gear icon for settings" className={styles.nav_img} height='40'/>
+                    <img src={gearSolid} alt="gear icon for settings" className={styles.nav_img} height='35'/>
                 </li>
             </ul>
         </nav>
