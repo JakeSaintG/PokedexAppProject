eff042a6ad9d3ec66f3371d9bd087347fcacffb6
JakeSaintG <jakesaint27@gmail.com>
2025-07-14 23:40:12 -0400

    keep working on 3 svgs in a trench coat

--- BaseDex/src/components/DexHeader/DexHeader.tsx
+++ BaseDex/src/components/DexHeader/DexHeader.tsx
@@ -1,5 +1,7 @@
 import styles from './DexHeader.module.css';
-import dexHeaderSvg from "../../assets/combined_dexheader.svg";
+import dexHeaderLeft from "../../assets/dexheader_left.svg";
+import dexHeaderMiddle from "../../assets/dexheader_middle_fill.svg";
+import dexHeaderRight from "../../assets/dexheader_right.svg";
 
 export function DexHeader( ) {
     return (
@@ -9,7 +11,11 @@ export function DexHeader( ) {
         // Everything in between should connect the two and would stretch with
         // different screen sizes.
         <div className={styles.dex_header}>
-            <img src={dexHeaderSvg} alt="Pokedex header with lights" />
+            {/* TODO: Wrap these three SVGs in a single alt text if possible */}
+            {/* Or aria hide them...because they don't really serve a functional purpose */}
+            <img src={dexHeaderLeft} className={styles.dex_left} alt="Pokedex header with lights" />
+            <img src={dexHeaderMiddle} className={styles.dex_middle} alt="Pokedex header with lights" />
+            <img src={dexHeaderRight} className={styles.dex_right} alt="Pokedex header with lights" />
         </div>
     );
 }
