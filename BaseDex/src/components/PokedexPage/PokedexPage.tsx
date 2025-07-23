ad39ad1fb4674ce51a05a864d42fc5ad02b387c6
JakeSaintG <jakesaint27@gmail.com>
2025-07-22 23:01:06 -0400

    start working through dex page

--- BaseDex/src/components/PokedexPage/PokedexPage.tsx
+++ BaseDex/src/components/PokedexPage/PokedexPage.tsx
@@ -2,11 +2,98 @@ import styles from './PokedexPage.module.css';
 import { DexHeader } from "../DexHeader";
 import { NavigationMenu } from "../NavigationMenu";
 
+const test_data = [
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "pikachu",
+        "dex_no": 25,
+        "id": 25
+    },
+    {
+        "name": "bulbasaur",
+        "dex_no": 1,
+        "id": 1
+    }
+]
+
 export function PokedexPage( ) {
     return (
         <div className={styles.pokedex}>
             <DexHeader></DexHeader>
-            <p>dex</p>
+            <div className={styles.dex_previews}>
+                {test_data.map((pkmn) => (
+                        <div>{pkmn.name}</div>
+                    ))}
+            </div>
             <NavigationMenu activePage='pokedex'></NavigationMenu>
         </div>
     );
