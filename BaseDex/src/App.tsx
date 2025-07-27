ca3b299d33c6618191b25edcefad44ab5e94db77
Jake St. Germain <jakesaint27@gmail.com>
2025-07-26 21:19:19 -0400

    start integrating postgresql into app

--- BaseDex/src/App.tsx
+++ BaseDex/src/App.tsx
@@ -1,16 +1,26 @@
 import { Outlet } from "react-router-dom";
 import styles from "./App.module.css";
 
+import { PGlite } from "@electric-sql/pglite";
+import { live } from "@electric-sql/pglite/live";
+import { PGliteProvider } from "@electric-sql/pglite-react";
+
+const dbContext = await PGlite.create({
+    extensions: { live },
+});
+
 export default function App() {
     return (
         /*
             Restricting myself to mobile-only views since this the whole point of 
             this is to stub-out making a mobile application.
-        */ 
-        <div className={styles.mobile_container}>
-            <div className={styles.app}>
-                <Outlet />
+        */
+        <PGliteProvider db={dbContext}>
+            <div className={styles.mobile_container}>
+                <div className={styles.app}>
+                    <Outlet />
+                </div>
             </div>
-        </div>
+        </PGliteProvider>
     );
 }
