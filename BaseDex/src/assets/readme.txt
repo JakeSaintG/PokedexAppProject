9325103db88864bc62a72e2b4acfcce5f753c8c0
Jake St. Germain <jakesaint27@gmail.com>
2025-07-27 18:48:49 -0400

    Finish up base dex front welcome page

---
+++ BaseDex/src/assets/readme.txt
@@ -0,0 +1,13 @@
+Making and tweaking the Pokemon font:
+- Use this font generator to create the yellow piece
+  - fontsz.com/pokemon-font-generator
+- Increase the canvas size
+  - Image -> Canvas Size 
+  - height 155px
+  - center yellow text horizontally
+
+- Making the outline
+  - outline: 5px
+  - color: 2E6DB4
+  - fill in the O's
+  - 
--- BaseDex/src/components/WelcomePage/WelcomePage.tsx
+++ BaseDex/src/components/WelcomePage/WelcomePage.tsx
@@ -4,6 +4,7 @@ import styles from './WelcomePage.module.css';
 import reactFont from '../../assets/reactfont.png'
 import pokdexFont from '../../assets/pokedexfont.png'
 import reactSvg from '../../assets/react.svg'
+import dexCoverTriangle from '../../assets/dex_cover_triangle.svg'
 
 export function WelcomePage() {
     const navigate = useNavigate();
@@ -13,12 +14,12 @@ export function WelcomePage() {
         <div onClick={loadData} className={styles.load}>
             <DexHeader></DexHeader>
             <div className={styles.welcome}>
-                {/* TODO: styles these with the blue outline and add the triangle on the right */}
-                {/* TODO: change this to grid and tweak the sizes */}
-                {/* TODO: make the other lang images: fontsz.com/pokemon-font-generator*/}
-                <img className={styles.font_image}src={reactSvg} alt='stylized font of the word "react"'/>
-                <img className={styles.font_image}src={reactFont} alt='stylized font of the word "react"'/>
-                <img className={styles.font_image}src={pokdexFont} alt='stylized font of the word "PokéDex"'/>
+                <div>
+                    <img className={`${styles.font_image} ${styles.react_logo}`}src={reactSvg} alt='React Logo of a blue atom with three orbits.'/>
+                    <img className={styles.font_image}src={reactFont} alt='Stylized font of the word "react".'/>
+                    <img className={styles.font_image}src={pokdexFont} alt='Stylized font of the word "PokéDex".'/>
+                </div>
+                <img className={styles.dex_triangle}src={dexCoverTriangle} alt='stylized font of the word "PokéDex"'/>
                 <p>Touch to begin!</p>
             </div>
         </div>
