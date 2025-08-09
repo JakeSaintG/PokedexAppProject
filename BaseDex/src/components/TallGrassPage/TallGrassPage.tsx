a2b331f171be90bf0cc3d66c3e5abba8317a8876
Jake St. Germain <jakesaint27@gmail.com>
2025-08-08 21:34:47 -0400

    start working on tall grass style

--- BaseDex/src/components/TallGrassPage/TallGrassPage.tsx
+++ BaseDex/src/components/TallGrassPage/TallGrassPage.tsx
@@ -1,16 +1,35 @@
-import styles from './TallGrassPage.module.css';
+import styles from "./TallGrassPage.module.css";
 import { DexHeader } from "../DexHeader";
 import { NavigationMenu } from "../NavigationMenu";
 
-export function TallGrassPage( ) {
+const test_data = [
+    "kanto",
+    "johto",
+    "hoenn",
+    "sinnoh",
+    "unova",
+    "kalos",
+    "alola",
+    "galar",
+    "hisui",
+    "paldea",
+];
+
+export function TallGrassPage() {
+    let key = 0;
+
     return (
         <div className={styles.tall_grass}>
-            <DexHeader></DexHeader>
-            <p>tall grass</p>
-            <div>
-                <a href=""></a>
+            <DexHeader/>
+            <h2>tall grass!</h2>
+            <div className={styles.regions}>
+                {test_data.map((r) => (
+                    <div className={styles.region} key={key++}>
+                        <p>{r}</p>
+                    </div>
+                ))}
             </div>
-            <NavigationMenu activePage='tall_grass'></NavigationMenu>
+            <NavigationMenu activePage="tall_grass"></NavigationMenu>
         </div>
     );
 }
