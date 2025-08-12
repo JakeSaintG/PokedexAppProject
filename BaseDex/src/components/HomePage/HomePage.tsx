a36b11df87c63db6243c607e53562aa60bac9030
Jake St. Germain <jakesaint27@gmail.com>
2025-08-11 21:52:05 -0400

    brief work on home page

--- BaseDex/src/components/HomePage/HomePage.tsx
+++ BaseDex/src/components/HomePage/HomePage.tsx
@@ -1,13 +1,23 @@
-import styles from './HomePage.module.css';
+import styles from "./HomePage.module.css";
 import { DexHeader } from "../DexHeader";
 import { NavigationMenu } from "../NavigationMenu";
 
-export function HomePage( ) {
+export function HomePage() {
     return (
-        <div className={styles.home}>
-            <DexHeader/>
-            <p>HOME</p>
-            <NavigationMenu activePage='home'></NavigationMenu>
-        </div>
+        <>
+            <DexHeader />
+            <div className={styles.home}>
+                <div className={styles.stats}>
+                    <p>
+                        Here I'll show some stats like how many pkmn are registered
+                    </p>
+                </div>
+                <p>HOME</p>
+                {/* TODO: If no load has occured due to previous offline state,
+                show a button here to try again */}
+                <button>try again</button>
+            </div>
+            <NavigationMenu activePage="home"></NavigationMenu>
+        </>
     );
 }
