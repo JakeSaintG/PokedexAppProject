a160fc0a7c1ee89fe1b1a5ceeced3c41a6c61c74
Jake St. Germain <jakesaint27@gmail.com>
2025-09-21 00:19:45 -0400

    Working on displaying blob as img src

--- BaseDex/src/components/PokedexPage/PokedexPreview/PokedexPreview.tsx
+++ BaseDex/src/components/PokedexPage/PokedexPreview/PokedexPreview.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import type { PokedexPreviewData } from '../../../types/pokdexPreviewData';
 import styles from './PokedexPreview.module.css';
 import { Link } from 'react-router-dom';
@@ -7,11 +8,20 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
 }
 
 export function PokedexPreview(props: Props) {
+
+    const [imageSrc, setImageSrc] = useState('');
+    
+    useEffect(() => {
+        const url = URL.createObjectURL(props.previewData.img_data);
+        setImageSrc(url);
+        return () => URL.revokeObjectURL(url);
+    }, [])
+    
     return (
         <Link className={styles.pokedex_preview} to={`/pokedex/entry?id=${props.previewData.id}`}>
             <section id={`${props.previewData.id}`}>
                 <p>{props.previewData.dex_no} </p>
-                <img src={URL.createObjectURL(props.previewData.img_data)} alt="placeholder dex image" />
+                <img src={imageSrc} alt="placeholder dex image" />
                 <p>{props.previewData.name} </p>
             </section>
         </Link>
