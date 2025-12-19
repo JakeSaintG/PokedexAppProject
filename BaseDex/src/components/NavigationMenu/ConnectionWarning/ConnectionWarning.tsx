daf6257711f0edd341dad9a43bd7bbff1be90ed8
JakeSaintG <jakesaint27@gmail.com>
2025-12-19 11:12:59 -0500

    continued work on error handling

--- BaseDex/src/components/NavigationMenu/ConnectionWarning/ConnectionWarning.tsx
+++ BaseDex/src/components/NavigationMenu/ConnectionWarning/ConnectionWarning.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from './ConnectionWarning.module.css';
 
 export function ConnectionWarning() {
 
     return (
         <div className={styles.con_warn}>
-            <p>Connection Warning</p>
+            <p>Warning!</p>
+            <span>This POC app seems to have lost stored data. Please return to <Link to='../'>start</Link> or reload data in settings.</span>
         </div>
     );
 }
