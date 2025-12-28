2c03aea10c36b3b370832b5dc395eaacbecd505b
Jake St. Germain <jakesaint27@gmail.com>
2025-12-27 19:21:45 -0500

    bake optional page title into dex header

--- BaseDex/src/components/DexHeader/DexHeader.tsx
+++ BaseDex/src/components/DexHeader/DexHeader.tsx
@@ -1,10 +1,25 @@
 import styles from './DexHeader.module.css';
 import dexHeaderLeft from "../../assets/dexheader/dexheader_left.svg";
 
-export function DexHeader( ) {
+interface Props extends React.HTMLAttributes<HTMLElement>{
+    title?: string
+}
+
+const displayTitle = (title: string | undefined) => {
+    if (title) {
+        return <h2 className={styles.page_title}>{title}</h2>
+    }
+
+    return <div className={styles.dex_header_white_space}></div>
+}
+
+export function DexHeader(props: Props) {
     return (
-        <div className={styles.dex_header}>
-            <img src={dexHeaderLeft} className={styles.dex_header_left} alt="Pokedex header with lights" />
-        </div>
+        <>
+            {displayTitle(props.title)}
+            <div className={styles.dex_header}>
+                <img src={dexHeaderLeft} className={styles.dex_header_left} alt="Pokedex header with lights" />
+            </div>
+        </>
     );
 }
