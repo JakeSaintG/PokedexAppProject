391cb59764f4a5cc6230c476400be17116ce9193
JakeSaintG <jakesaint27@gmail.com>
2026-01-22 23:41:39 -0500

    flesh out home page a little (sick day...)

--- BaseDex/src/components/Pages/HomePage/HomePage.tsx
+++ BaseDex/src/components/Pages/HomePage/HomePage.tsx
@@ -18,10 +18,19 @@ export function HomePage() {
         <>
             <DexHeader title="Home"/>
             <div className={styles.home}>
+                <h3>Welcome!</h3>
                 <div className={styles.stats}>
-                    <p>
-                        Here I'll show some stats like how many pkmn are registered
-                    </p>
+                    {/* make permanently dismissable - persist in settings table; add setting to bring tutorial back!*/}
+                    <div className={styles.tutorial}>
+                        <p>Head over to the tall grass to begin registering Pokemon!</p>
+                        <p>Once you have a few, check them out in the Pokedex!</p>
+                        <p>Tap the person icon to customize your profile.</p>
+                        <p>Head to settings to tweak you experience, enable secret features, and add more regions to explore (when ready)!</p>
+                    </div>
+                    <div className={styles.total_registered}>
+                        <p>Total registered:</p>
+                        <p>0/0</p>
+                    </div>
                 </div>
             </div>
             <NavigationMenu activePage="home" connectionError={dbError}></NavigationMenu>
